6d7b3bae57b396acbc019e13e59d4c8ff75704e3
Rossini fonseca <rossinidrum@outlook.com>
2024-04-15 23:06:50 -0300

    feat: Add validations to Customer model
    Add validations to the Customer model to ensure data integrity and security

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ApplicationRecord
   has_secure_password
 
+  validates :name, presence: { message: "Name required" }
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: { message: "Password required" }, length: { minimum: 6, message: "Password must be at least 6 characters long" }
 
 end
