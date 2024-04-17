53bcb8f884bf694acfb18509eafab1194572e08c
Rossini fonseca <rossinidrum@outlook.com>
2024-04-17 19:14:06 -0300

    feat: Adiciona funcionalidade de recuperação de senha com Mailer
    Detalhes:
    - Implementa a lógica para solicitar a recuperação de senha.
    - Adiciona o mailer para enviar o link de redefinição de senha.
    - Refatora as rotas para incluir novos endpoints relacionados à recuperação de senha.
    - Refatora as rotas para incluir novos endpoints relacionados à recuperação de senha.

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,39 @@
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @customer = Customer.find_by(email:params[:email_address])
+      PasswordMailer.with(customer: @customer).reset.deliver_later
+      render json: { message: "Email sent with password reset instructions." }, status: :ok
+    else
+      render json: { error: "Email address not found." }, status: :not_found
+    end
+  end
+
+
+  def update
+
+
+    @Customer = Customer.find_signed!(params[:token], purpose: 'password_reset')
+
+    if @Customer.update(password_params)
+     return render json: { message: "password changed successfully" }, status: :ok
+    else
+      render json: { message: "failed to change password" }, status: :unprocessable_entity
+    end
+
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+
+    render json: { message: "failed to change password" }, status: :unprocessable_entity
+
+  end
+
+  private
+
+  def password_params
+    params.permit(:password)
+  end
+
+
+end
