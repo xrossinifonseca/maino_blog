cb1d8b2976ff82f5dbce38701589e569594c63e4
Rossini fonseca <rossinidrum@outlook.com>
2024-04-17 20:46:11 -0300

    feat: add update_password method to handle customer password changes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,23 +1,63 @@
 class CustomersController < ApplicationController
 
-
+  include JwtAuthentication
 
 
   def create
     begin
-      Customer.transaction do
         @customer = Customer.create!(customer_params)
         render json: {message:"User registered successfully", customer: @customer.as_json(except: :password_digest)}, status: :created
-      end
-    rescue ActiveRecord::RecordInvalid => e
+     rescue ActiveRecord::RecordInvalid => e
       render json: {error:e.message}, status: :unprocessable_entity
-    rescue => e
+     rescue => e
       p e
       render json: {error:"there was an internal failure"}, status: :internal_server_error
     end
   end
 
+
+
+  def update_password
+
+      begin
+          @customer = authenticated_customer
+
+          if @customer.nil?
+            return
+          end
+
+      new_password = params["new_password"]
+
+      unless new_password
+        return render json: { error: "New password is missing" }, status: :unprocessable_entity
+      end
+
+      @customer.password = new_password
+
+      if @customer.save!
+        render json: { message: "Password changed successfully" }, status: :ok
+      else
+        render json: { error: @customer.errors.full_messages.join(", ") }, status: :unprocessable_entity
+      end
+
+      rescue => e
+        render json: { error: e.message }, status: :internal_server_error
+      end
+
+  end
+
+
   private
+
+
+  def authenticated_customer
+    auth = authenticate(cookies)
+
+    if auth
+        Customer.find(auth["customer_id"])
+    end
+
+  end
 
   def customer_params
     params.permit(:name,:email,:password)
