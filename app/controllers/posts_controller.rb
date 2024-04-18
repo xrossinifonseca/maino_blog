8831b9bccea6def542b62742035e2f9efc142460
Rossini fonseca <rossinidrum@outlook.com>
2024-04-18 13:38:27 -0300

    feat: add destroy method to handle deleting a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,11 @@ class PostsController < ApplicationController
   include JwtAuthentication
 
 
+  before_action :authenticate_customer, only: [:create, :destroy]
+
+
   def create
-
     begin
-      @customer = authenticated_customer
-      if @customer.nil?
-        return
-      end
-
       Post.transaction do
         post = Post.create!(post_params.merge(author_id: @customer.id))
         render json: {message:"Post created successfully", post: post }, status: :created
@@ -22,11 +19,35 @@ class PostsController < ApplicationController
       p e
       render json: {error:"there was an internal failure"}, status: :internal_server_error
     end
-
   end
 
 
+  def destroy
+    begin
+      Post.transaction do
+        post = Post.find(params["id"])
+        post.destroy
+        render json: { message: "Post successfully deleted" }, status: :ok
+      end
+
+    rescue ActiveRecord::RecordInvalid => e
+      render json: {error:e.message}, status: :unprocessable_entity
+    rescue => e
+      render json: {error:e.message}, status: :unprocessable_entity
+    end
+  end
+
+
+
+
   private
+  def authenticate_customer
+    @customer = authenticated_customer
+    if @customer.nil?
+      return
+    end
+  end
+
   def post_params
     params.permit(:title,:content)
   end
