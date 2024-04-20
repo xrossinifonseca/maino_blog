82f61534ef3fdb61410931d1a24a189e0f26e3f4
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 16:27:56 -0300

    feat: implements login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,77 @@
-<script setup></script>
+<script setup>
+import { reactive, ref } from "vue";
+import Input from "../components/Input.vue";
+import Button from "../components/Button.vue";
+import { RouterLink } from "vue-router";
+import { authenticate } from "../auth";
+
+const values = reactive({
+  email: "",
+  password: "",
+});
+
+const errors = reactive({ ...values });
+const loading = ref(false);
+
+const onSubmit = async () => {
+  errors.email = "";
+  errors.password = "";
+
+  if (!values.password || !values.email) {
+    if (!values.email) {
+      errors.email = "E-mail inválido";
+    }
+    if (!values.password) {
+      errors.password = "Necessário informar sua senha.";
+    }
+    return;
+  }
+  try {
+    loading.value = true;
+    await authenticate(values);
+  } catch (error) {
+    if (error.message === "invalid credentials") {
+      errors.email = "Email ou senha inválidos";
+    }
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
 
 <template>
-  <div>Login</div>
+  <div class="flex justify-center">
+    <div class="w-full max-w-[600px] p-5 bg-white space-y-5 py-20">
+      <h1 class="font-bold text-3xl text-blue-primary text-center">Login</h1>
+
+      <form class="space-y-10" @submit.prevent="onSubmit">
+        <Input
+          v-model="values.email"
+          autocomplete="email"
+          placeholder="E-mail"
+          name="email"
+          :error="errors.email"
+        />
+        <div>
+          <Input
+            type="password"
+            v-model="values.password"
+            placeholder="Senha"
+            autocomplete="current-password"
+            name="password"
+            :error="errors.password"
+          />
+          <RouterLink
+            to="recover-password"
+            class="text-sm font-medium text-slate-500"
+          >
+            Esqueceu sua senha?
+          </RouterLink>
+        </div>
+        <div class="flex justify-center">
+          <Button :loading="loading" type="submit"> Entrar </Button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
