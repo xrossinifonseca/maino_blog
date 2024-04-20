d2cd93754ab4e1d8ccd884757cefff0935728bdd
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 12:46:23 -0300

    feat: add method getPosts

--- client/src/service/axios.js
+++ client/src/service/axios.js
@@ -4,3 +4,19 @@ const service = axios.create({
   baseURL: "https://mainoblog-production.up.railway.app/api/v1",
   withCredentials: true,
 });
+
+export const getPosts = async (page, tags) => {
+  let path = `posts?page=${page}`;
+
+  if (tags) {
+    let query = tags
+      .split(",")
+      .flatMap((item) => {
+        return item.trim().includes(" ") ? item.trim().split(" ") : item.trim();
+      })
+      .join("+");
+    path = `posts?tags=${query}&page=${page}`;
+  }
+
+  return service.get(path);
+};
