140714488628e1eb85584a20045370e4c5b2117d
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 16:26:17 -0300

    add routerLink

--- client/src/components/posts/CardPost.vue
+++ client/src/components/posts/CardPost.vue
@@ -1,30 +1,32 @@
 <script setup>
-defineProps(["img", "title", "author", "date", "tags"]);
+defineProps(["img", "title", "author", "date", "tags", "id"]);
 </script>
 
 <template>
   <div class="w-full max-w-[390px] space-y-4">
-    <div class="w-full bg-gray-300 h-[320px] rounded-xl"></div>
+    <RouterLink :to="'/post/' + id">
+      <div class="w-full bg-gray-300 h-[320px] rounded-xl"></div>
 
-    <div>
-      <h1 class="text-start text-lg font-bold text-slate-800">
-        {{ title }}
-      </h1>
-    </div>
+      <div>
+        <h1 class="text-start text-lg font-bold text-slate-800">
+          {{ title }}
+        </h1>
+      </div>
 
-    <ul class="flex flex-wrap gap-2 items-center">
-      <li
-        class="p-2 bg-slate-300 rounded-xl text-sm text-blue-primary"
-        v-for="tag in tags"
-        :key="tag.id"
-      >
-        {{ tag.name }}
-      </li>
-    </ul>
+      <ul class="flex flex-wrap gap-2 items-center" v-if="tags.length > 0">
+        <li
+          class="p-2 bg-slate-300 rounded-xl text-sm text-blue-primary"
+          v-for="tag in tags"
+          :key="tag.id"
+        >
+          {{ tag.name }}
+        </li>
+      </ul>
 
-    <div class="flex items-center gap-2 justify-between text-slate-600">
-      <h2>{{ author }}</h2>
-      <h3>{{ date }}</h3>
-    </div>
+      <div class="flex items-center gap-2 justify-between text-slate-600">
+        <h2>{{ author }}</h2>
+        <h3>{{ date }}</h3>
+      </div>
+    </RouterLink>
   </div>
 </template>
