a1da296c6cba8dfc8f667c718cb835e67de833fc
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 16:29:18 -0300

    export files

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -3,3 +3,6 @@ export { default as Login } from "./Login.vue";
 export { default as MyPosts } from "./MyPosts.vue";
 export { default as Layout } from "./Layout.vue";
 export { default as Signup } from "./Signup.vue";
+export { default as RecoverPassword } from "./RecoverPassword.vue";
+export { default as NewPost } from "./NewPost.vue";
+export { default as Post } from "./Post.vue";
