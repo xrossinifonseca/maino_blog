08562e4095646f525d92b67c26a2a3f7aac244be
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 16:25:29 -0300

    handle if the customer is authenticated

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,6 +1,14 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import Button from "./Button.vue";
+import { isAuthenticated, customer } from "../auth";
+import { ref } from "vue";
+
+const drop = ref(false);
+
+const toggleDrop = () => {
+  drop.value = !drop.value;
+};
 </script>
 
 <template>
@@ -13,13 +21,55 @@ import Button from "./Button.vue";
       >
     </div>
 
-    <ul class="flex items-center gap-4">
-      <RouterLink to="login" class="font-bold text-xl text-slate-800"
-        >Login</RouterLink
+    <div v-show="!isAuthenticated">
+      <ul class="flex items-center gap-4">
+        <RouterLink to="login" class="font-bold text-xl text-slate-800"
+          >Login</RouterLink
+        >
+        <RouterLink to="signup">
+          <Button> Cadastrar </Button>
+        </RouterLink>
+      </ul>
+    </div>
+
+    <div
+      v-if="isAuthenticated && customer"
+      class="flex gap-2 items-center relative cursor-pointer"
+      @click="toggleDrop"
+    >
+      <h1 class="text-xl text-blue-primary font-medium">
+        Olá,
+        <span class="capitalize">{{ customer?.name }}</span>
+      </h1>
+
+      <div :data-open="drop" class="w-4 rotate-180 data-[open=true]:rotate-0">
+        <svg
+          fill="#000000"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+
+      <div
+        :data-open="drop"
+        class="absolute text-blue-primary w-full px-2 bg-white top-10 data-[open=true]:h-28 h-0 flex flex-col gap-1 items-start overflow-y-hidden"
       >
-      <RouterLink to="signup">
-        <Button> Cadastrar </Button>
-      </RouterLink>
-    </ul>
+        <RouterLink to="my-posts" class="mt-2">Meus Posts</RouterLink>
+        <RouterLink>Minha conta</RouterLink>
+        <button>Sair</button>
+      </div>
+    </div>
   </div>
 </template>
