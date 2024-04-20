66abf9ec7ae7d778e0ac24d229e51a1055f281a5
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 16:24:12 -0300

    includes loading props

--- client/src/components/Button.vue
+++ client/src/components/Button.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+defineProps(["loading"]);
+</script>
 
 <template>
   <button
     v-bind="$attrs"
     class="flex items-center justify-center text-white bg-blue-primary py-2 px-4 rounded"
   >
-    <slot></slot>
+    <span v-if="loading"> Loading.. </span>
+
+    <span v-else>
+      <slot></slot>
+    </span>
   </button>
 </template>
