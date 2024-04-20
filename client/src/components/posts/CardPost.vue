0ccb73d7572c5fab4b027f40316a124da5327725
Rossini fonseca <rossinidrum@outlook.com>
2024-04-20 12:45:36 -0300

    feat: create cardPost component to reflect information from a post

--- client/src/components/posts/CardPost.vue
+++ client/src/components/posts/CardPost.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps(["img", "title", "author", "date"]);
+defineProps(["img", "title", "author", "date", "tags"]);
 </script>
 
 <template>
@@ -11,6 +11,16 @@ defineProps(["img", "title", "author", "date"]);
         {{ title }}
       </h1>
     </div>
+
+    <ul class="flex flex-wrap gap-2 items-center">
+      <li
+        class="p-2 bg-slate-300 rounded-xl text-sm text-blue-primary"
+        v-for="tag in tags"
+        :key="tag.id"
+      >
+        {{ tag.name }}
+      </li>
+    </ul>
 
     <div class="flex items-center gap-2 justify-between text-slate-600">
       <h2>{{ author }}</h2>
