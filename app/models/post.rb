74b56bafca403fd144298fb6b2bb79d0b7da2023
Rossini fonseca <rossinidrum@outlook.com>
2024-04-19 23:23:52 -0300

    feat: add   association has_many PostTags and has_many Tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "Customer", foreign_key: "author_id"
   has_many :comments, dependent: :destroy
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 
 
   validates :title, presence: { message: "required" }
   validates :content,  presence: { message: "required" }
-
 
 
 end
