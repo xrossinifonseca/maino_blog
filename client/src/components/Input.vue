2e13786c50c6d5167cc3920e87cfbe9ec58de666
Rossini fonseca <rossinidrum@outlook.com>
2024-04-21 03:00:36 -0300

    reactor styles

--- client/src/components/Input.vue
+++ client/src/components/Input.vue
@@ -9,7 +9,7 @@ const emit = defineEmits(["update:modelValue"]);
       :value="modelValue"
       v-bind="$attrs"
       @input="emit('update:modelValue', $event.target.value)"
-      class="p-2 w-full outline-none bg-transparent border-b-2"
+      class="p-2 w-full outline-none bg-transparent border-b-2 disabled:border-none"
     />
     <span v-if="error" class="text-medium text-red-500 text-lg">{{
       error
