c69d211b1a87781d99368b28f2e2dbfe781cdc26
Rossini fonseca <rossinidrum@outlook.com>
2024-04-21 04:27:55 -0300

    update origin cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-      origins "https://blog.rossinicode.com/"
+      origins "https://rossinicode.com/","https://blog.rossinicode.com/","http://localhost:3002/"
       resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
